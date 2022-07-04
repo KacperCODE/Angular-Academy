8743c4404c94557fb77e16193e0053c3636a77ba
clhk <clhk@viessmann.com>
2022-07-04 12:23:10 +0200

    NGA-002-Final code for part 2

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,12 @@ import { ButtonComponent } from './button/button.component';
 import { InputComponent } from './input/input.component';
 import { MaterialModule } from '../material/material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
-  imports: [CommonModule, BrowserAnimationsModule, MaterialModule],
+  imports: [CommonModule, BrowserAnimationsModule, MaterialModule, FormsModule, ReactiveFormsModule, MatCheckboxModule],
   declarations: [ButtonComponent, InputComponent],
-  exports: [ButtonComponent, InputComponent],
+  exports: [ButtonComponent, InputComponent, BrowserAnimationsModule, MaterialModule, FormsModule, ReactiveFormsModule, MatCheckboxModule],
 })
 export class SharedModule {}
