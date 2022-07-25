bae896297aea3714bb02d79a04b79c755851ccd2
Jakub Roszkowski <rsjk@viessmann.com>
2022-07-25 10:55:08 +0200

    Init user data splitting

---
+++ src/app/overview/user-overview/user-badge/user-badge.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-user-badge",
+  templateUrl: "./user-badge.component.html",
+  styleUrls: ["./user-badge.component.css"],
+})
+export class UserBadgeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/overview/user-overview/user-overview.component.ts
+++ src/app/overview/user-overview/user-overview.component.ts
@@ -1,37 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { UserDetails } from '../../new-user/create-account/create-account.component';
+import { Component, OnInit } from "@angular/core";
+import { UserDetails } from "../../new-user/create-account/create-account.component";
 
 @Component({
-  selector: 'app-user-overview',
-  templateUrl: './user-overview.component.html',
-  styleUrls: ['./user-overview.component.css']
+  selector: "app-user-overview",
+  templateUrl: "./user-overview.component.html",
+  styleUrls: ["./user-overview.component.css"],
 })
 export class UserOverviewComponent implements OnInit {
+  selectedUserIndex: number;
 
   users: UserDetails[] = [
     {
-      "name": "Jan",
-      "surname": "Kowalski",
-      "email": "jankowalski@gmail.com",
-      "tosAccepted": true
-  },
-  {
-    "name": "Krystyna",
-    "surname": "Zgazownik",
-    "email": "krysia@gmail.com",
-    "tosAccepted": true
-  },
-  {
-    "name": "Kamil",
-    "surname": "Tumulec",
-    "email": "tumulec@gmail.com",
-    "tosAccepted": true
-  }
+      name: "Jan",
+      surname: "Kowalski",
+      email: "jankowalski@gmail.com",
+      tosAccepted: true,
+    },
+    {
+      name: "Krystyna",
+      surname: "Zgazownik",
+      email: "krysia@gmail.com",
+      tosAccepted: true,
+    },
+    {
+      name: "Kamil",
+      surname: "Tumulec",
+      email: "tumulec@gmail.com",
+      tosAccepted: true,
+    },
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  setSelectedUserIndex(index: number): void {
+    this.selectedUserIndex = index;
   }
-
 }
